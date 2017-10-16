c80403f25dd3d75521fb0fc1c708d05b9c56dfea
Jake Halloran <jakehalloran2@gmail.com>
2017-10-16 15:15:47 -0400

    final

---
+++ BasicGit.java
@@ -0,0 +1,8 @@
+public class BasicGit{
+  public static final int ITERATIONS=10;
+  public static void main(String[] args){
+    for(int i=1;i<=ITERATIONS;i++){
+      System.out.println(i);
+    }
+  }
+}
